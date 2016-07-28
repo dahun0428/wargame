b4b685ef3ca9d2b7e8e0930f41bfa45b67db8742
Dahun Lee <dahun0428@gmail.com>
2016-07-28 22:38:50 +0900

    add probs category

--- wargame/probs/models.py
+++ wargame/probs/models.py
@@ -6,6 +6,13 @@ from django.utils import timezone
 # Create your models here.
 
 class Problem (models.Model):
+  PWNABLE = 'PWN'
+  REVERSING = 'REV'
+  WEB = 'WEB'
+  FORENSIC = 'FRS'
+  CRYPTO = 'CRT'
+  
+
   prob_title = models.CharField (max_length=200)
   prob_description = models.TextField ()
   created_date = models.DateTimeField (default=timezone.now)
@@ -13,6 +20,19 @@ class Problem (models.Model):
   prob_flag = models.CharField (max_length = 100)
   prob_image = models.ImageField ()
   prob_file = models.FileField ()
+
+  PROB_CATEGORY = (
+    (PWNABLE, 'pwnable'),
+    (REVERSING, 'reversing'),
+    (WEB, 'web'),
+    (FORENSIC, 'forensic'),
+    (CRYPTO, 'crypto'),
+  )
+  prob_category = models.CharField (
+    max_length = 3,
+    choices = PROB_CATEGORY,
+    default = PWNABLE,
+  )
 
   def __str__ (self):
     return self.prob_title
